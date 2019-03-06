575d32419f4ea295122133069bcfcba810931e03
neptune <tadhgbgblackman@gmail.com>
2019-03-07 00:23:15 +0800

    Gluth base scripts

--- DBM-Naxx/ConstructQuarter/Gluth.lua
+++ DBM-Naxx/ConstructQuarter/Gluth.lua
@@ -9,7 +9,8 @@ mod:RegisterCombat("combat")
 mod:EnableModel()
 
 mod:RegisterEvents(
-	"SPELL_DAMAGE"
+	"SPELL_DAMAGE",
+	"PLAYER_ALIVE"
 )
 
 
@@ -20,6 +21,7 @@ local enrageTimer		= mod:NewBerserkTimer(420)
 local timerDecimate		= mod:NewCDTimer(104, 54426)
 
 function mod:OnCombatStart(delay)
+	self:ScheduleMethod(0, "getBestKill")
 	enrageTimer:Start(420 - delay)
 	timerDecimate:Start(110 - delay)
 	warnDecimateSoon:Schedule(100 - delay)
@@ -35,4 +37,20 @@ function mod:SPELL_DAMAGE(args)
 	end
 end
 
+function mod:OnCombatEnd(wipe)
+	self:Stop();
+end
 
+function mod:PLAYER_ALIVE()
+	if UnitIsDeadOrGhost("PLAYER") and self.Options.ResetOnRelease then
+		self:Stop();
+	end
+end
+
+---------- SPEED KILL FUNCTION ----------
+local timerSpeedKill		= mod:NewTimer(5, "Fastest Kill")
+function mod:getBestKill()
+	local bestkillTime = (mod:IsDifficulty("heroic5", "heroic25") and mod.stats.heroicBestTime) or mod:IsDifficulty("normal5", "heroic10") and mod.stats.bestTime
+	timerSpeedKill:Show(bestkillTime)
+end
+---------- SPEED KILL FUNCTION ----------
