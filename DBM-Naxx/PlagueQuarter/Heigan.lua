d8acf092cdc46b5d0bf6c47bff36f62e60a6e752
neptune <tadhgbgblackman@gmail.com>
2019-03-07 00:26:53 +0800

    Heigan base scripts

--- DBM-Naxx/PlagueQuarter/Heigan.lua
+++ DBM-Naxx/PlagueQuarter/Heigan.lua
@@ -6,6 +6,10 @@ mod:SetCreatureID(15936)
 
 mod:RegisterCombat("combat")
 
+mod:RegisterEvents(
+	"PLAYER_ALIVE"
+)
+
 mod:EnableModel()
 
 mod:RegisterEvents()
@@ -16,6 +20,7 @@ local warnTeleportNow	= mod:NewAnnounce("WarningTeleportNow", 3, 46573)
 local timerTeleport		= mod:NewTimer(90, "TimerTeleport", 46573)
 
 function mod:OnCombatStart(delay)
+	self:ScheduleMethod(0, "getBestKill")
 	mod:BackInRoom(90 - delay)
 end
 
@@ -32,3 +37,21 @@ function mod:BackInRoom(time)
 	warnTeleportNow:Schedule(time)
 	self:ScheduleMethod(time, "DancePhase")
 end
+
+function mod:OnCombatEnd(wipe)
+	self:Stop();
+end
+
+function mod:PLAYER_ALIVE()
+	if UnitIsDeadOrGhost("PLAYER") and self.Options.ResetOnRelease then
+		self:Stop();
+	end
+end
+
+---------- SPEED KILL FUNCTION ----------
+local timerSpeedKill		= mod:NewTimer(5, "Fastest Kill")
+function mod:getBestKill()
+	local bestkillTime = (mod:IsDifficulty("heroic5", "heroic25") and mod.stats.heroicBestTime) or mod:IsDifficulty("normal5", "heroic10") and mod.stats.bestTime
+	timerSpeedKill:Show(bestkillTime)
+end
+---------- SPEED KILL FUNCTION ----------
