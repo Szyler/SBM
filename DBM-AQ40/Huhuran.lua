d80828a4ace5ab6f9963358b1f10f8bd6f35cc85
neptune <tadhgbgblackman@gmail.com>
2019-04-02 22:49:05 +0800

    1.4: Huhuran add soft enrage tracking

--- DBM-AQ40/Huhuran.lua
+++ DBM-AQ40/Huhuran.lua
@@ -25,6 +25,8 @@ end
 
 local warnSoon		= mod:NewAnnounce("Adds Soon", 2)
 local warnSpawn		= mod:NewAnnounce("Adds Spawned", 3)
+local prewarnSoftEnrage		= mod:NewAnnounce("Soft Enrage Soon", 2)
+local warnSoftEnrage		= mod:NewAnnounce("Soft Enrage Now", 3)
 local timerSpawn	= mod:NewTimer(60, "Adds")
 
 local warnSting			= mod:NewTargetAnnounce(26180, 2)
@@ -38,12 +40,16 @@ local specWarnAcid		= mod:NewSpecialWarningStack(26050, nil, 10)
 
 local berserkTimer	=	mod:NewBerserkTimer(420)
 local StingTargets = {}
+local check1
+local check2
 
 function mod:OnCombatStart(delay)
 	berserkTimer:Start(-delay)	
 	self:ScheduleMethod(0-delay, "initialSpawn")
 	table.wipe(StingTargets)
 	self:ScheduleMethod(0, "getBestKill")
+	check1 = 0
+	check2 = 0
 end
 
 function mod:Soon()
@@ -95,6 +101,18 @@ mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 function mod:SPELL_AURA_REMOVED(args)
 	if args:IsSpellID(26180) then
 		timerSting:Cancel()
+	end
+end
+
+function mod:UNIT_HEALTH(args)
+    huhuranHealth = math.max(0, UnitHealth("boss1")) / math.max(1, UnitHealthMax("boss1")) * 100;
+	
+	if huhuranHealth < 35 and check1 == 0 then
+		check1 = 1
+		prewarnSoftEnrage:Show()
+	elseif skeramHealth < 30 and check2 == 0 then
+		check2 = 1
+		warnSoftEnrage:Show()
 	end
 end
 
