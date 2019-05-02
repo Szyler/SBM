38815568462073971bec6072c14904a46bd1421a
neptune <tadhgbgblackman@gmail.com>
2019-05-03 00:17:28 +0800

    Resolved an issue with OBM Pull and OBM Break

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -822,16 +822,19 @@ SlashCmdList["DEADLYBOSSMODS"] = function(msg)
 		if DBM:GetRaidRank() == 0 then
 			return DBM:AddMsg(DBM_ERROR_NO_PERMISSION)
 		end
+		local delay, mod, revision = strsplit("\t", msg)
+		local lag = select(3, GetNetStats()) / 1000
+		delay = tonumber(delay or 0) or 0
 		local timer = tonumber(cmd:sub(5)) or 10
 		local channel = ((GetNumRaidMembers() == 0) and "PARTY") or "RAID_WARNING"
 		DBM:CreatePizzaTimer(timer, DBM_CORE_TIMER_PULL, true)
 		SendChatMessage(DBM_CORE_ANNOUNCE_PULL:format(timer), channel)
-		if timer > 7 then DBM:Schedule(timer - 7, SendChatMessage, DBM_CORE_ANNOUNCE_PULL:format(7), channel) end
-		if timer > 5 then DBM:Schedule(timer - 5, SendChatMessage, DBM_CORE_ANNOUNCE_PULL:format(5), channel) end
-		if timer > 3 then DBM:Schedule(timer - 3, SendChatMessage, DBM_CORE_ANNOUNCE_PULL:format(3), channel) end
-		if timer > 2 then DBM:Schedule(timer - 2, SendChatMessage, DBM_CORE_ANNOUNCE_PULL:format(2), channel) end
-		if timer > 1 then DBM:Schedule(timer - 1, SendChatMessage, DBM_CORE_ANNOUNCE_PULL:format(1), channel) end
-		DBM:Schedule(timer, SendChatMessage, DBM_CORE_ANNOUNCE_PULL_NOW, channel)
+		if timer > 7 then DBM:Schedule(timer - 7 - delay - lag, SendChatMessage, DBM_CORE_ANNOUNCE_PULL:format(7), channel) end
+		if timer > 5 then DBM:Schedule(timer - 5 - delay - lag, SendChatMessage, DBM_CORE_ANNOUNCE_PULL:format(5), channel) end
+		if timer > 3 then DBM:Schedule(timer - 3 - delay - lag, SendChatMessage, DBM_CORE_ANNOUNCE_PULL:format(3), channel) end
+		if timer > 2 then DBM:Schedule(timer - 2 - delay - lag, SendChatMessage, DBM_CORE_ANNOUNCE_PULL:format(2), channel) end
+		if timer > 1 then DBM:Schedule(timer - 1 - delay - lag, SendChatMessage, DBM_CORE_ANNOUNCE_PULL:format(1), channel) end
+		DBM:Schedule(timer - delay - lag, SendChatMessage, DBM_CORE_ANNOUNCE_PULL_NOW, channel)
 	elseif cmd:sub(1, 5) == "arrow" then
 		if not DBM:IsInRaid() then
 			DBM:AddMsg(DBM_ARROW_NO_RAIDGROUP)
